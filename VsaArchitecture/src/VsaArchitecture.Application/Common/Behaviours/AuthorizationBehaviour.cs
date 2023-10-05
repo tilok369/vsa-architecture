bfc5e2abd7fb0a8fa09cfa0b247f66a96faee2d2
Tanjeer <tbh.tilok@live.com>
2023-10-05 15:50:52 +0600

    pipeline behaviors in progress

---
+++ VsaArchitecture/src/VsaArchitecture.Application/Common/Behaviours/AuthorizationBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System.Reflection;
+using VsaArchitecture.Application.Common.Security;
+using VsaArchitecture.Application.Contracts.Authentication;
+
+namespace VsaArchitecture.Application.Common.Behaviours;
+
+public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    private readonly ICurrentUserService _currentUserService;
+
+    public AuthorizationBehaviour(ICurrentUserService currentUserService)
+    {
+        _currentUserService = currentUserService;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var authorizeAttributes = request?.GetType().GetCustomAttributes<AuthorizeAttribute>();
+
+        if (authorizeAttributes?.Any() ?? false)
+        {
+            // Must be authenticated user
+            if (_currentUserService.UserId == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            //TODO: check roles and policies in authorizeAttributes and do the necessary authorization
+        }
+
+        // User is authorized / authorization not required
+        return await next();
+    }
+}
---
+++ VsaArchitecture/src/VsaArchitecture.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Runtime.CompilerServices;
+using VsaArchitecture.Application.Contracts.Authentication;
+
+namespace VsaArchitecture.Application.Common.Behaviours;
+
+public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+{
+    private readonly ILogger _logger;
+    private readonly ICurrentUserService _currentUserService;
+
+    public LoggingBehaviour(ICurrentUserService currentUserService, ILogger<TRequest> logger)
+    {
+        _currentUserService = currentUserService;
+        _logger = logger;
+    }
+
+    public Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+        var userId = _currentUserService.UserId ?? string.Empty;
+
+        return Task.Run(() => _logger.LogInformation("VsaArchitecture Request: {Name} {@UserId} {@Request}",
+                requestName, userId, request), cancellationToken);
+    }
+}
